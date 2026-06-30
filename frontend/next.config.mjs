dd32321612a2ab326f9e21904eb25eb5a9b2160d
k4nul <kojjok971@naver.com>
2026-06-30 15:40:36 +0900

    chore(frontend): 8080 개발 서버 설정 추가

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.0.11", "dev.k4nul.com"],
   typescript: {
     ignoreBuildErrors: true,
   },
